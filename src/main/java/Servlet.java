670007e579fa5368a69aae93f4747d441521da2c
UndMund <zavadskiy.nazar@mail.ru>
2023-03-22 19:37:37 +0300

    Feature-2 change view

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -16,20 +16,13 @@ public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
         //resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        resp.setHeader("MYHEADER", "Hi");;
+        resp.setHeader("MYHEADER", "Hi");
+        ;
 
         String param1 = req.getParameter("param1");
 
-        String[] ages = req.getParameterValues("age");
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        try (PrintWriter writer = resp.getWriter();){
-            parameterMap.entrySet().
-                    forEach(e -> writer.println(
-                            "<p>key: " +e.getKey() + " value: " + Arrays.toString(e.getValue()) + "</p>"));
-            writer.println("<h1>ages = " + ages[0] + "</h1>");
-            writer.println("<h1>param1 = " + param1 + "</h1>");
-            writer.println("<br/><a href=\"second\">Открыть Second Servlet</a>");
-        }
 
     }
+
 }
+
